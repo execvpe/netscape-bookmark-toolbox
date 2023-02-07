7e1064b5c570796c72f4b872a71e772831f3623b
Martin Weinzierl <152305632+execvpe@users.noreply.github.com>
2023-02-07 22:05:23 +0100

    Disable debug print statement

--- compare_lines.py
+++ compare_lines.py
@@ -16,7 +16,7 @@ def main(argc: int, argv: list[str]) -> None:
         for l in f.readlines():
             set2.add(l.strip())
 
-    print(f"===== X is in {argv[1]}, but not in {argv[2]} =====")
+#   print(f"===== X is in {argv[1]}, but not in {argv[2]} =====")
     [print(x) for x in set1 if x not in set2]
 #   print(f"===== X is in {argv[2]}, but not in {argv[1]} =====")
 #   [print(x) for x in set2 if x not in set1]
